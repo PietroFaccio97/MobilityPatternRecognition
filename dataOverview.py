92131c346c0bbaa7b27a9875bd4d1d31c78374ec
FaceTcd1997 <117104430+FaceTcd1997@users.noreply.github.com>
2023-02-22 11:17:09 +0000

    Switch to Object-Oriented implementation

--- dataOverview.py
+++ dataOverview.py
@@ -1,15 +1,21 @@
-import readData
+from Classes.dataReader import DataReader
 from Static.typeEnum import Paths
 import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 from collections import Counter
 
-bus = readData.retrieveData(Paths.bus)
-car = readData.retrieveData(Paths.car)
-pedestrian = readData.retrieveData(Paths.pedestrian)
-static = readData.retrieveData(Paths.static)
-train = readData.retrieveData(Paths.train)
+# Columns to read
+columns = ['ue_ident', 'timestamp', 'phy_ul_pucch_rssi', 'phy_ul_pucch_rssi']
+patterns = [Paths.bus, Paths.car, Paths.pedestrian, Paths.static, Paths.train]
+
+reader = DataReader(columns, patterns)
+
+bus = reader.retrievePatternData(Paths.bus)
+car = reader.retrievePatternData(Paths.car)
+pedestrian = reader.retrievePatternData(Paths.pedestrian)
+static = reader.retrievePatternData(Paths.static)
+train = reader.retrievePatternData(Paths.train)
 
 busSize = len(Counter(bus['ue_ident']).keys())
 carSize = len(Counter(car['ue_ident']).keys())
